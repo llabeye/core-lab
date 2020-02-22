fbb188405e3a93b01d12c3c551d7e344cebd6522
llabeye <allodola@hotmail.co.uk>
2020-02-21 22:41:08 -0500

    Rev

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $(function(){
+     $(".proj1").draggable();
+   } );
+  $(function(){
+      $(".proj2").draggable();
+    } );
+   $(function(){
+     $(".proj3").draggable();
+   } );
+    $(function(){
+     $(".proj4").draggable();
+   } );
+     $(function(){
+     $(".proj5").draggable();
+   } );
+      $(function(){
+     $(".proj6").draggable();
+   } );
+       $(function(){
+     $(".proj7").draggable();
+   } );
+
+   } );
